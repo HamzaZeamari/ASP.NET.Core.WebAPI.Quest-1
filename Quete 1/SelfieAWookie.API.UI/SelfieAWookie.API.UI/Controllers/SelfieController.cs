b6fb8272042ad58eed1532a3ccbc0b7302e35470
Hamza Zeamari <zeam.hamza@gmail.com>
2022-09-17 01:14:59 +0200

    Quest 1: Initializing of project

---
+++ Quete 1/SelfieAWookie.API.UI/SelfieAWookie.API.UI/Controllers/SelfieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SelfieAWookie.API.UI.Models;
+
+namespace SelfieAWookie.API.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SelfieController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Selfie> Get()
+        {
+            return Enumerable.Range(1,10).Select(item => new Selfie() { Id = item });
+        }
+    }
+}
---
+++ Quete 1/SelfieAWookie.API.UI/SelfieAWookie.API.UI/Models/Selfie.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfieAWookie.API.UI.Models
+{   
+    /// <summary>
+    /// Représente un selfie avec un wookie
+    /// </summary>
+    public class Selfie
+    {
+        public int Id { get; set; }
+    }
+}
